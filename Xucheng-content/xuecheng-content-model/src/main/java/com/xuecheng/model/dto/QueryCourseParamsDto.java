309517c3642b8e8fe75e19b0d793279c530d0647
lebrwcd <lebrwcd@zpp.com>
2023-02-05 13:20:03 +0800

    1.初始化内容管理模块的模块结构 2.代码生成器

---
+++ Xucheng-content/xuecheng-content-model/src/main/java/com/xuecheng/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,24 @@
+package com.xuecheng.model.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2022/10/7 16:08
+ */
+@Data
+public class QueryCourseParamsDto {
+
+    //审核状态
+    @ApiModelProperty("审核状态")
+    private String auditStatus;
+    //课程名称
+    @ApiModelProperty("课程名称")
+    private String courseName;
+    //发布状态
+    @ApiModelProperty("发布状态")
+    private String publishStatus;
+}
